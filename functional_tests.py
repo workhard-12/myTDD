85e70aea513c09b25b710e2d72190da5e800bc27
孙秋成 <978235849@qq.com>
2021-02-22 21:11:24 +0800

    第二章

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver 
-
-browser = webdriver.Firefox() 
-browser.get('http://localhost:8000') 
+import unittest 
  
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase): #➊ 
+ 
+    def setUp(self): #➋ 
+        self.browser = webdriver.Firefox() 
+ 
+    def tearDown(self): #➌ 
+        self.browser.quit() 
+ 
+    def test_can_start_a_list_and_retrieve_it_later(self): #➍ 
+        # 伊迪丝听说有一个很酷的在线待办事项应用 
+        # 她去看了这个应用的首页 
+        self.browser.get('http://localhost:8000') 
+ 
+        # 她注意到网页的标题和头部都包含 “To-Do” 这个词 
+        self.assertIn('To-Do', self.browser.title) #➎ 
+        self.fail('Finish the test!') #➏ 
+ 
+if __name__ == '__main__': #➐ 
+    unittest.main(warnings='ignore') #➑
