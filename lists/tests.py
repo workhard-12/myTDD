5d3d4207e6d0da4642978d94d53bb35c18b61150
孙秋成 <978235849@qq.com>
2021-02-23 21:37:14 +0800

    3.2

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,9 @@
+from django.urls import resolve 
 from django.test import TestCase 
-class SmokeTest(TestCase): 
-    def test_bad_maths(self): 
-        self.assertEqual(1 + 1, 3)
+from lists.views import home_page #➊ 
+ 
+class HomePageTest(TestCase): 
+ 
+    def test_root_url_resolves_to_home_page_view(self): 
+        found = resolve('/') #➋ 
+        self.assertEqual(found.func, home_page) #➌
