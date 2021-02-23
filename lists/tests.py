6407ee51ec3beeae9165b8611d8949a6bdbce5ed
孙秋成 <978235849@qq.com>
2021-02-23 21:45:21 +0800

    3.3

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,19 @@
 from django.urls import resolve 
 from django.test import TestCase 
-from lists.views import home_page #➊ 
+from django.http import HttpRequest 
+ 
+from lists.views import home_page 
  
 class HomePageTest(TestCase): 
  
     def test_root_url_resolves_to_home_page_view(self): 
-        found = resolve('/') #➋ 
-        self.assertEqual(found.func, home_page) #➌
+        found = resolve('/') 
+        self.assertEqual(found.func, home_page) 
+ 
+    def test_home_page_returns_correct_html(self): 
+        request = HttpRequest() #➊ 
+        response = home_page(request) #➋ 
+        self.assertTrue(response.content.startswith(b'<html>')) #➌ 
+        self.assertIn(b'<title>To-Do lists</title>', response.content) #➍ 
+        self.assertTrue(response.content.endswith(b'</html>')) #➎
+
