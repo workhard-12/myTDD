34c351456566fe9dc5bf1a16ffe93dd16e5e2857
孙秋成 <978235849@qq.com>
2021-03-04 21:29:30 +0800

    5.4

--- lists/tests.py
+++ lists/tests.py
@@ -24,10 +24,32 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
+
     def test_can_save_a_POST_request(self): 
+        self.client.post('/',data={'item_text':'A new list item'})
+        
+        self.assertEqual(Item.objects.count(), 1) #➊ 
+        new_item = Item.objects.first() #➋ 
+        self.assertEqual(new_item.text, 'A new list item') #➌ 
+
+    def test_redirects_after_POST(self): 
         response=self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response,'home.html')
+        self.assertEqual(response.status_code, 302) 
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self): 
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self): 
+        Item.objects.create(text='itemey 1') 
+        Item.objects.create(text='itemey 2') 
+ 
+        response = self.client.get('/')
+ 
+        self.assertIn('itemey 1', response.content.decode()) 
+        self.assertIn('itemey 2', response.content.decod())
+        
 class ItemModelTest(TestCase): 
  
     def test_saving_and_retrieving_items(self): 
