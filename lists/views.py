e5e1f38752790ac7323848d03054ad5d5a6e7d9d
孙秋成 <978235849@qq.com>
2021-03-08 21:55:33 +0800

    7.2

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from lists.models import Item 
  
-def home_page(request): 
-    if request.method == 'POST': 
-        Item.objects.create(text=request.POST['item_text']) 
-        return redirect('/') 
- 
-    items = Item.objects.all() 
-    return render(request, 'home.html', {'items': items})
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from lists import views
  
 urlpatterns = [
-    # Examples: 
     url(r'^$', views.home_page, name='home'),
-    # url(r'^blog/', include('blog.urls')), 
-    # url(r'âdmin/', include(admin.site.urls)), 
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
