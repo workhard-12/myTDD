5f4b5c2630ddb66ba2794fa31e9932e7676b486f
孙秋成 <978235849@qq.com>
2021-03-08 22:40:53 +0800

    7.4

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,6 @@
 from django.db import models
-
-class Item(models.Model): 
-    text=models.TextField(default='')
+class List(models.Model):
+    pass
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List,on_delete=models.CASCADE,default=None)
